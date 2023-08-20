bda97378c330edbaec99297ebfbb782dc0963fac
fabian <godsonhm3@gmail.com>
2023-08-20 10:01:39 +0100

    add api documentation using YAML and swaggerUI

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger and yaml
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // connectDB
 const connectDB = require("./db/connect");
 
@@ -34,8 +39,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) =>
+  res.send(`<h1>JOBS API</h1><a href="/api-docs">Api Documentation</a>`)
+);
+app.use("api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
-app.get("/", (req, res) => res.send(`<h1>JOBS API</h1>`));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
